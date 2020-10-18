dbcbd711dc31058ce20e7127f7652addba6921c8
Akinus <gabriel.akonom@gmail.com>
2020-10-17 21:58:11 -0700

    Added automatic generation of editable network map using yEd.

--- testing/pyscan.py
+++ testing/pyscan.py
@@ -16,6 +16,8 @@ from datetime import datetime
 from itertools import repeat
 import subprocess as sub
 from subprocess import STDOUT, check_output
+from re import search
+import pyyed
 import time
 import socket
 import threading
@@ -132,14 +134,23 @@ def parseRange(nputstr=""):
         print("Invalid set: " + str(invalid))
     return selection
 
+def get_ip_address(net):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((net + '.0', 80))
+    return s.getsockname()[0]
+
 def addSubnet(addy):
     with lock:
+        pivot = get_ip_address(addy)
         tree = ET.parse('pyscan.xml')
         root = tree.getroot()
         newSN = ET.SubElement(root, 'subnet')
-        newSN.set('address', addy)
-        newSN.set('pivot', "")
-        newSN.set('name', "")
+        newaddy = ET.SubElement(newSN, 'subnet-address')
+        newaddy.text = addy
+        newpivot = ET.SubElement(newSN, 'pivot')
+        newpivot.text = pivot
+        newname = ET.SubElement(newSN, 'subnet-name')
+        newname.text = ""
         indent(root)
         tree.write("pyscan.xml")
     return
@@ -147,21 +158,24 @@ def addSubnet(addy):
 def addHost(addy):
     with lock:
         sl = addy.split('.')
-        subnetstr = './subnet[@address = "{0}.{1}.{2}"]'.format(sl[0], sl[1], sl[2])
+        subnetstr = './subnet/[subnet-address = "{0}.{1}.{2}"]'.format(sl[0], sl[1], sl[2])
+        pivotstr = './subnet/[subnet-address = "{0}.{1}.{2}"]/pivot'.format(sl[0], sl[1], sl[2])
         tree = ET.parse('pyscan.xml')
         root = tree.getroot()
         subnet = root.find(subnetstr)
-        newip = ET.SubElement(subnet, 'host')
-        newaddr = ET.SubElement(newip, 'address')
-        newaddr.text = addy
-        newhn = ET.SubElement(newip, 'hostname')
-        newhn.text = ''
-        newun = ET.SubElement(newip, 'username')
-        newun.text = ''
-        newpw = ET.SubElement(newip, 'password')
-        newpw.text = ''
-        indent(root)
-        tree.write("pyscan.xml")
+        pivot = root.find(pivotstr).text
+        if addy != pivot:
+            newip = ET.SubElement(subnet, 'host')
+            newaddr = ET.SubElement(newip, 'address')
+            newaddr.text = addy
+            newhn = ET.SubElement(newip, 'hostname')
+            newhn.text = ''
+            newun = ET.SubElement(newip, 'username')
+            newun.text = ''
+            newpw = ET.SubElement(newip, 'password')
+            newpw.text = ''
+            indent(root)
+            tree.write("pyscan.xml")
     return
 
 def addPort(addy, num, banner):
@@ -170,14 +184,34 @@ def addPort(addy, num, banner):
         tree = ET.parse('pyscan.xml')
         root = tree.getroot()
         host = root.find(addyStr)
-        if host.find('./port/[number="'+str(num)+'"]') == None:
-            newport = ET.SubElement(host, 'port')
-            newportnum = ET.SubElement(newport, 'number')
-            newportnum.text = str(num)
-            newportbanner = ET.SubElement(newport, 'banner')
-            newportbanner.text = banner
-            indent(root)
-            tree.write("pyscan.xml")
+        if search('ssh', banner):
+            # print('SSH PORT FOUND!!')
+            if host.find('./port/[number="'+str(num)+'"]') == None:
+                newport = ET.SubElement(host, 'port')
+                newportnum = ET.SubElement(newport, 'number')
+                newportnum.text = str(num)
+                newportbanner = ET.SubElement(newport, 'banner')
+                newportbanner.text = banner
+                newtunnel = ET.SubElement(newport, 'tunnel')
+                newtunnellport = ET.SubElement(newtunnel, 'local-port')
+                newtunnellport.text = ""
+                newtunneltarget = ET.SubElement(newtunnel, 'tunnel-target')
+                newtunneltarget.text = ""
+                newtunneltport = ET.SubElement(newtunnel, 'tunnel-target-port')
+                newtunneltport.text = ""
+                newtunnelbuild = ET.SubElement(newtunnel, 'existing-tunnel-port')
+                newtunnelbuild.text = ""
+                indent(root)
+                tree.write("pyscan.xml")
+        else:
+            if host.find('./port/[number="'+str(num)+'"]') == None:
+                newport = ET.SubElement(host, 'port')
+                newportnum = ET.SubElement(newport, 'number')
+                newportnum.text = str(num)
+                newportbanner = ET.SubElement(newport, 'banner')
+                newportbanner.text = banner
+                indent(root)
+                tree.write("pyscan.xml")
     return
 
 def bannerGrab(addy, port):
@@ -296,15 +330,19 @@ def initiate():
                                                 ***If this option is enabled, you cannot use -e or --end.***")
     uinput.add_argument("-p", "--ports", help = "The ports to be scanned. Should be comma-separated or can be a range ie: 1-30.\
                                                 Defaults to list from: https://rb.gy/x86g6c")
-    uinput.add_argument("-n", "--new", help = "Clears the log and starts fresh.", action="store_true")
+    uinput.add_argument("-c", "--clearlog", help = "Clears the log and starts fresh.", action="store_true")
     uinput.add_argument("-f", "--fast", help = "Performs a fast scan using netcat vs the default /dev/tcp.  This option does have \
                                                 the potential to miss some ports.  REQUIRES NETCAT to be installed. \
                                                 ", action="store_true")
-    uinput.add_argument("--show", help = "Shows the currently logged results for the address.  Nothing more", action="store_true")
+    uinput.add_argument("--show", help = "Shows the currently logged results for the address.  When used with --map \
+                                            this will recreate the network map also", action="store_true")
+    uinput.add_argument("-m", "--map", help = "Creates a network map to a .png file", action="store_true")
 
     ipstart = 1
     ipend = 254
     fulladd = False
+    clearlog = False
+    netmap = False
 
     opts = uinput.parse_args()
 
@@ -312,6 +350,7 @@ def initiate():
     net = opts.address
     if opts.show:
         printall(net)
+        netgraph()
         sys.exit()
 
     if len(net.split('.')) > 3:
@@ -398,12 +437,9 @@ def initiate():
     print(phostvar)
     print('Scanning ports {0}'.format(pports))
         
-    if opts.new:
-        newScan()
-
     tree = ET.parse('pyscan.xml')
     root = tree.getroot()
-    subnetstr = './subnet[@address = "'+net+'"]'
+    subnetstr = './subnet/[subnet-address = "'+net+'"]'
     subnet = root.find(subnetstr)
     if subnet == None:
         addSubnet(net)
@@ -419,8 +455,76 @@ def initiate():
         printnet = '{0}.{1}'.format(net, min(final_range))
     else:
         printnet = net
+
+    if opts.map:
+        netmap = True
+    
+    if opts.clearlog:
+        clearlog = True
                 
-    return operation, net, final_range, final_ports, totalscans, printnet
+    return operation, net, final_range, final_ports, totalscans, printnet, clearlog, netmap
+
+def netgraph():
+    print('\nCreating network map to pyscan.graphml in current directory...')
+    print('Download yEd to edit pyscan.graphml from https://www.yworks.com/products/yed')
+    G = pyyed.Graph()
+    # f = plt.figure()
+
+    bn = 'base'
+    G.add_node(bn, label='base')
+
+    options = {
+    'with_labels': True,
+    'node_size': 100,
+    'font_size': 1,
+    'node_shape': 's'
+    }
+
+    tree = ET.parse('pyscan.xml')
+    root = tree.getroot()
+    subnets = root.findall('subnet')
+    for sub in subnets:
+        rd = sub.findtext('pivot')
+        sa = sub.findtext('subnet-address')
+        sn = sub.findtext('subnet-name')
+
+        subnetText = '{0}\n{1}'.format(sa, sn)
+
+        G.add_node(sa, label=subnetText, shape="roundrectangle")
+        G.add_edge(bn, sa, label=get_ip_address(sa))
+        hosts = sub.findall('host')
+        for h in hosts:
+            portnums = 'Ports:'
+            addy = h.find('address').text
+            ports = h.findall('port')
+            hostname = h.find('hostname').text
+            counter = 0
+            for p in ports:
+                var = p.findtext('number')
+                if counter == 5:
+                    portnums = '{0}\n{1}'.format(portnums, var)
+                    counter = 1
+                elif counter == 0:
+                    portnums = '{0} {1}'.format(portnums, var)
+                    counter += 3
+                else:
+                    portnums = '{0}, {1}'.format(portnums, var)
+                    counter += 1
+            if hostname == None:
+                hostname = 'Hostname Unknown'
+            nodeText = '{0}\n{1}\n{2}'.format(addy, hostname, portnums)
+            G.add_node(addy, label=nodeText, shape="roundrectangle")
+            G.add_edge(sa, addy)
+    
+    # pos = nx.spring_layout(G)
+    # nx.draw(G, pos, **options)
+    # nx.write_graphml(G, "test.graphml")
+    # f.savefig("pyscan.png", dpi=1200)
+    with open('pyscan.graphml', 'w') as fp:
+        fp.write(G.get_graph())
+
+    print('Complete!')
+    return
 
 def sortXML(addy):
     if len(addy.split('.')) > 3:
@@ -431,7 +535,7 @@ def sortXML(addy):
 
     tree = ET.parse('pyscan.xml')
     root = tree.getroot()
-    subnetstr = './subnet[@address = "'+naddy+'"]'
+    subnetstr = './subnet/[subnet-address = "'+naddy+'"]'
     subnet = root.find(subnetstr)
     subnethosts = subnet.findall('host')
     if subnethosts:
@@ -440,7 +544,7 @@ def sortXML(addy):
         pdata = []
         tree = ET.parse('pyscan.xml')
         root = tree.getroot()
-        subnetstr = './subnet[@address = "'+naddy+'"]'
+        subnetstr = './subnet/[subnet-address = "'+naddy+'"]'
         subnet = root.find(subnetstr)
         subnethosts = subnet.findall('host')
         for host in subnethosts:            
@@ -476,55 +580,55 @@ def printall(addy):
 
         tree = ET.parse('pyscan.xml')
         root = tree.getroot()
-        subnetstr = './subnet[@address = "'+naddy+'"]'
+        subnetstr = './subnet/[subnet-address = "'+naddy+'"]'
         subnet = root.find(subnetstr)
         subnethosts = subnet.findall('host')
         if subnethosts:
             plist = list()
             print('\n---------------')
             print(ip)
-            for p in root.findall('./subnet[@address = "'+naddy+'"]/host/[address = "'+ip+'"]/port/number'):
+            for p in root.findall('./subnet/[subnet-address = "'+naddy+'"]/host/[address = "'+ip+'"]/port/number'):
                 # print('     {0}'.format(p.text))
                 plist.append(int(p.text))
             for pp in sorted(plist):
                 printtext = '|__ {0}'.format(pp)
-                for b in root.findall('./subnet[@address = "'+naddy+'"]/host/[address = "'+ip+'"]/port/[number = "'+str(pp)+'"]/banner'):
+                for b in root.findall('./subnet/[subnet-address = "'+naddy+'"]/host/[address = "'+ip+'"]/port/[number = "'+str(pp)+'"]/banner'):
                     if b.text:
                         banner = b.text
                         printtext = '|__ {0}\t--> {1}'.format(pp, banner)
-                print(printtext) 
+                print(printtext[:65]) 
                 plist.remove(pp)       
     else:
         naddy = addy
         tree = ET.parse('pyscan.xml')
         root = tree.getroot()
-        subnetstr = './subnet[@address = "'+naddy+'"]'
+        subnetstr = './subnet/[subnet-address = "'+naddy+'"]'
         subnet = root.find(subnetstr)
         subnethosts = subnet.findall('host')
         if subnethosts:
             hlist = list()
             plist = list()
-            for h in root.findall('./subnet[@address = "'+naddy+'"]/host/address'):
+            for h in root.findall('./subnet/[subnet-address = "'+naddy+'"]/host/address'):
                 hlist.append(h.text)
             ip_list = [ip.strip() for ip in hlist]
             for ip in sorted(ip_list, key = lambda ip: ( int(ip.split(".")[0]), int(ip.split(".")[1]), int(ip.split(".")[2]), int(ip.split(".")[3]))):
                 print('\n---------------')
                 print(ip)
-                for p in root.findall('./subnet[@address = "'+naddy+'"]/host/[address = "'+ip+'"]/port/number'):
+                for p in root.findall('./subnet/[subnet-address = "'+naddy+'"]/host/[address = "'+ip+'"]/port/number'):
                     # print('     {0}'.format(p.text))
                     plist.append(int(p.text))
                 for pp in sorted(plist):
                     printtext = '|__ {0}'.format(pp)
-                    for b in root.findall('./subnet[@address = "'+naddy+'"]/host/[address = "'+ip+'"]/port/[number = "'+str(pp)+'"]/banner'):
+                    for b in root.findall('./subnet/[subnet-address = "'+naddy+'"]/host/[address = "'+ip+'"]/port/[number = "'+str(pp)+'"]/banner'):
                         if b.text:
                             banner = b.text
                             printtext = '|__ {0}\t--> {1}'.format(pp, banner)
-                    print(printtext)             
+                    print(printtext[:65])             
                     plist.remove(pp) 
  
     return
 
-def newScan():
+def clearLog():
     now = datetime.now()
     dt_string = now.strftime("%Y%m%d%H%M%S")
     with open("pyscan.xml", "w") as f:
@@ -532,6 +636,15 @@ def newScan():
         f.write('<scan>\n')
         f.write('</scan>')
         f.close()
+    return
+
+def newScan(addy):
+    tree = ET.parse('pyscan.xml')
+    root = tree.getroot()
+    subnetstr = './subnet/[subnet-address = "'+addy+'"]'
+    subnet = root.find(subnetstr)
+    if subnet == None:
+        addSubnet(addy)
     return
 
 def init(args):
@@ -558,13 +671,20 @@ if __name__ == '__main__':
     final_ports = scanInfo[3]
     totalscans = scanInfo[4]
     printnet = scanInfo[5]
+    clearlog = scanInfo[6]
+    netmap = scanInfo[7]
 
     contVar = input('Continue? Y/N (default y): ')
     if contVar != 'Y' and contVar != 'Yes' and contVar != 'yes' and contVar != 'y' and contVar != '':
         sys.exit(2)
+    
+    if clearlog:
+        clearLog()
+    
+    newScan(net)
+
     Timer()
     update_progress()
-
     try:
         for i in final_range:
             addy = str(net) + "." + str(i)
@@ -582,6 +702,8 @@ if __name__ == '__main__':
             sys.exit(2)
         else:
             printall(printnet)
+            if netmap:
+                netgraph()
             sys.exit(2)
 
     currcount.value = totalscans
@@ -594,6 +716,8 @@ if __name__ == '__main__':
         sys.exit(2)
     else:
         printall(printnet)
+        if netmap:
+            netgraph()
     
 
 #--------------------------------------------------------------------------
